1646e852790011c487a0e2a1fff812b20d23967e
Sansar Maske <sansarmaske@gmail.com>
2025-01-14 01:00:46 -0500

    add expenses menu and create expense page

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('expenses')" :active="request()->routeIs('expenses')">
+                        {{ __('Expenses') }}
+                    </x-nav-link>
                 </div>
             </div>
 
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/expenses', function () {return view('expenses.index');})->name('expenses');
+    Route::get('/expenses/create', function () {return view('expenses.create');})->name('expenses.create');
 });
 
-require __DIR__.'/auth.php';
+
+
+require __DIR__ . '/auth.php';
