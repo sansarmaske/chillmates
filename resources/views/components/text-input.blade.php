8ddf47877fa7c4c1076390ecd44feee7b84bbd07
Sansar Maske <sansarmaske@gmail.com>
2025-02-19 14:11:16 -0500

    refactor: use text-input component on create expense form and refactor text-input to accept text-area as a parameter

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,7 @@
-@props(['disabled' => false])
+@props(['disabled' => false, 'textarea' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) }}>
+@if($textarea)
+    <textarea rows="5" @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) }}></textarea>
+@else
+    <input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) }}>
+@endif
